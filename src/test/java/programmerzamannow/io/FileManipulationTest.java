10634fd1ac2bf9d1340b96c5d8cf425643fedf8a
Eko Kurniawan Khannedy <echo.khannedy@gmail.com>
2023-03-26 11:39:39 +0700

    File Manipulation Test

---
+++ src/test/java/programmerzamannow/io/FileManipulationTest.java
@@ -0,0 +1,22 @@
+package programmerzamannow.io;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class FileManipulationTest {
+
+    @Test
+    void fileManipulation() throws IOException {
+        Path path = Path.of("file.txt");
+        Files.createFile(path);
+        Assertions.assertTrue(Files.exists(path));
+
+        Files.delete(path);
+        Assertions.assertFalse(Files.exists(path));
+    }
+}
